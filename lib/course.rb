51d7d730d6f2c0ec25cc1d4cbd64e207ff8566aa
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-18 00:00:55 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,13 +1,19 @@
 class Course
+
   attr_accessor :title, :schedule, :description
+
   @@all = []
-  def all
-    @@all
-  end
-  def reset_all
-    @@all = []
-  end
+
   def initialize
     @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
 end
